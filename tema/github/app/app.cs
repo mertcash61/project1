2ea2c50227e46960b8be587997743dbde51b88cf
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-10-06 13:40:52 +0300

    Add files via upload

---
+++ tema/github/app/app.cs
@@ -0,0 +1,25 @@
+class app
+{
+    app(Parameters)
+    {
+       [Fact]
+       public void TestName()
+       {
+        // Given
+       checked
+       {
+        
+       }
+        // When
+        do
+        {
+            
+        } while (true);       
+        // Then
+        #region app
+           System.Windows.Forms.MessageBox.Show("Text");
+           System.WriteLine.Fact.MessageBox.async("Text")
+        #endregion
+       }    
+    }
+}
