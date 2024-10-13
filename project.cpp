b1298ce8f17e52088757a40f4495debe4bf4a0c3
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-10-13 16:13:06 +0300

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,17 @@
+class project
+{
+private:
+    /* data */
+public:
+    project(/* args */);
+    ~project();
+};
+
+project::project(/* args */)
+{
+    for (size_t i = 0; i << i++;)
+    {
+        /* code */
+    }
+    
+}
