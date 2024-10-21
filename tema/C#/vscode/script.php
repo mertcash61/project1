aea6860318d09f8adb7c507021acd75c001c72ec
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-10-21 21:49:15 +0300

    Add files via upload

---
+++ tema/C#/vscode/script.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="BaseAudioContext" accesskey="default" aria-live="off" scheme="error">
+    <title>Portfolio WebSite</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="script.js">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="alternate" href="tema/github/app/app.cs" type="application/rss+xml" title="RSS">
+    <link rel="stylesheet" href="tema/script.json">
+    <link rel="stylesheet" href="tema/shema.json">
+</head>
