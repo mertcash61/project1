0472e6df52352195815fa6fa244d602809b2f0fe
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-10-23 22:08:33 +0300

    Update main.php

--- tema/C#/vscode/main.php
+++ tema/C#/vscode/main.php
@@ -1,19 +1,1 @@
-<?php
 
-namespace
-
-{
-    $_REQUEST->$_FILES = $this->assertClassHasStaticAttribute($attributeName, $className);
-    $_COOKIE->$http_response_header = $this->assertIsWritable($filename);
-    
-    $this->assertDirectoryExists($_REQUEST);
-
-    
-
-    $_COOKIE->zip_open = $this->assertTrue($condition);
-    $_COOKIE->zip_close = $this->assertTrue($condition);
-
-    private static{
-        @$this->assertXmlStringEqualsXmlString($expectedXml, $actualXml);
-    }
-}
