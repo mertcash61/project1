a85385b6c4764b9f2ad30d6234b1d5460271509a
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-10-24 22:10:24 +0300

    Update script.php

--- tema/C#/vscode/script.php
+++ tema/C#/vscode/script.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="BaseAudioContext" accesskey="default" aria-live="off" scheme="error">
-    <title>Portfolio WebSite</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="script.js">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="alternate" href="tema/github/app/app.cs" type="application/rss+xml" title="RSS">
-    <link rel="stylesheet" href="tema/script.json">
-    <link rel="stylesheet" href="tema/shema.json">
-</head>
+<?php
+
+namespace
+
+/**
+ * undocumented class
+ * @param Type $var description
+ * @var type
+ */
+final class script extends AnotherClass implements Interface
+{
+    $this->assertDirectoryIsWritable($directory);
+    use const $this->assertSame($expected, $actual);;
+    define('JSON', 'CSS');
+    ($a <=> $b === 0)
+    $this->$_SERVER($this->assertArrayHasKey($key, $array);)
+}
